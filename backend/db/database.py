b32e7bdc5eec8bd5b7b4a5e0676ffd06579864b9
Morozov Danila <danmorozoff@mail.ru>
2025-10-28 00:19:52 +0300

    make data

--- backend/db/database.py
+++ backend/db/database.py
@@ -13,7 +13,6 @@ DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
